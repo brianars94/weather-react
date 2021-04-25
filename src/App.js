a34b58fad1f5965898d3606c3b1556b58a51cdf3
brianars94 <brianars@hawaii.edu>
2021-04-25 15:16:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherSearch from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          hELLO WORLD
+          <WeatherSearch />
+        </h1>
+            
+      </header>
+    </div>
+  );
+}
+
+export default App;
