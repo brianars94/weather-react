1336ed07a1ee4a62ac354e5e8f036f96405c36d9
brianars94 <brianars@hawaii.edu>
2021-04-25 15:47:01 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          hELLO WORLD
+          hELLO WORLD,, hi!
           <WeatherSearch />
         </h1>
             
